e10d36cbe538dae2452d2aa90df4fb40dbbbb1ca
Kevin Herrera <kevin@herrera.io>
2017-05-12 16:44:21 -0700

    Adding instantiators.

--- src/KHerGe/CollectionAccess/Exception/InvalidCollectionException.php
+++ src/KHerGe/CollectionAccess/Exception/InvalidCollectionException.php
@@ -9,4 +9,42 @@ namespace KHerGe\CollectionAccess\Exception;
  */
 class InvalidCollectionException extends CollectionAccessorException
 {
+    /**
+     * Creates a new exception for a collection that is not accessible.
+     *
+     * @param object $source     The source that has the collection.
+     * @param string $collection The name of the collection.
+     *
+     * @return InvalidCollectionException The new exception.
+     */
+    public static function notAccessible($source, string $collection) : self
+    {
+        return new self(
+            sprintf(
+                'The collection "%s" for "%s" is not accessible.',
+                $collection,
+                get_class($source)
+            )
+        );
+    }
+
+    /**
+     * Creates a new exception for a collection that is not array accessible.
+     *
+     *
+     * @param null|object $source     The source that has the collection.
+     * @param string      $collection The name of the collection.
+     *
+     * @return InvalidCollectionException The new exception.
+     */
+    public static function notArrayAccessible($source, string $collection) :self
+    {
+        return new self(
+            sprintf(
+                'The collection "%s"%s is not array accessible.',
+                $collection,
+                is_object($source) ? (' for "' . get_class($source)) : '"'
+            )
+        );
+    }
 }
--- src/KHerGe/CollectionAccess/Exception/InvalidSourceException.php
+++ src/KHerGe/CollectionAccess/Exception/InvalidSourceException.php
@@ -9,4 +9,20 @@ namespace KHerGe\CollectionAccess\Exception;
  */
 class InvalidSourceException extends CollectionAccessorException
 {
+    /**
+     * Creates a new exception for an invalid source.
+     *
+     * @param mixed $source The invalid source.
+     *
+     * @return InvalidSourceException The new exception.
+     */
+    public static function with($source) : self
+    {
+        return new self(
+            sprintf(
+                'Expected an array or object, received a %s.',
+                gettype($source)
+            )
+        );
+    }
 }
